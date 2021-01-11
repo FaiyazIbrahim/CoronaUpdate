77fc505cfa0e3a30ef168389621f52ca91d2188e
rootfiz <31610884+rootfiz@users.noreply.github.com>
2021-01-11 15:43:21 +0600

    all data is showing in console but not filtering

--- lib/main.dart
+++ lib/main.dart
@@ -4,35 +4,17 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Album> fetchAlbum() async {
-  final response = await http
-      .get('https://covid19-stats-api.herokuapp.com/api/v1/cases?country=');
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class Album {
-  final int cases;
-  final int recovered;
-  final int death;
-
-  Album({this.cases, this.recovered, this.death});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      cases: json['confirmed'],
-      recovered: json['recovered'],
-      death: json['deaths'],
-    );
-  }
+Future<int> fetchData() async {
+  String url = 'https://api.covid19api.com/summary';
+  var response = await http
+      .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+  print(response.body);
+  var extract = json.decode(response.body);
+  extract.toString();
+  //var data;
+  //data = extract["Message"];
+  //print(extract["Message"]);
+  print(extract["Message"]["Global"]["TotalConfirmed"]);
 }
 
 void main() => runApp(MyApp());
@@ -45,12 +27,10 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Future<Album> futureAlbum;
-
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    //futureAlbum = fetchData();
   }
 
   @override
@@ -65,26 +45,12 @@ class _MyAppState extends State<MyApp> {
           title: Text('Covid-19 Alert'),
           backgroundColor: Color.fromARGB(100, 50, 62, 45),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchData,
+        ),
         body: Padding(
           padding: const EdgeInsets.only(
               top: 200, bottom: 20, left: 152, right: 150),
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(
-                  snapshot.data.cases.toString(),
-                  style: TextStyle(fontSize: 20),
-                );
-              }
-              if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              // By default, show a loading spinner.
-
-              return CircularProgressIndicator();
-            },
-          ),
         ),
         // body: Center(
         //   child: FutureBuilder<Album>(
